82eb4003ae20d954bba16b948d532690ed855e25
JotellyBarros <jotelly@gmail.com>
2019-04-24 15:55:28 -0300

    Correction calibration

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//g++ -std=c++11 main.cpp -lstdc++ $(pkg-config --libs --cflags opencv)
+
 #include <cstdio>
 #include <iostream>
 
@@ -13,27 +15,28 @@ int main(int argc, char **argv)
 {
   // converts the image to hsv so that circle detection is more accurate
   Mat hsv_image;
-  Mat src;
+  Mat bgr_image;
 
-  src = imread("/home/jotellybarros/Documents/corroded_n1.jpeg", 1);
-  cvtColor(src, hsv_image, COLOR_BGR2HSV);
-
+  bgr_image = imread("images/all_corrosion.jpeg", 1);
   Mat imgThreshold1, imgThreshold2, imgThreshold;
-  inRange(src,
-          Scalar(30, 30, 40),
-          Scalar(80, 110, 160),
+
+  //Structure color
+  int chanel_r = 75;
+  int chanel_g = 52;
+  int chanel_b = 64;
+  int variation_min = 30;
+  int variation_max = 40;
+
+  inRange(bgr_image,
+          Scalar((chanel_b - variation_min), (chanel_g - variation_min), (chanel_r - variation_min)),
+          Scalar((chanel_b + variation_max), (chanel_g + variation_max), (chanel_r + variation_max)),
           imgThreshold1);
 
-  // inRange(src,
-  //         Scalar(30, 30, 40),
-  //         Scalar(80, 110, 160),
-  //         imgThreshold2);
-
-  //imgThreshold = max(imgThreshold1, imgThreshold2); // combining the two thresholds
   imgThreshold = imgThreshold1;
 
   Mat element_erode = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
   Mat element_dilate = getStructuringElement(MORPH_ELLIPSE, Size(10, 10));
+
   /// Apply the erosion and dilation operations
   erode(imgThreshold, imgThreshold, element_erode);
   dilate(imgThreshold, imgThreshold, element_dilate);
@@ -42,64 +45,19 @@ int main(int argc, char **argv)
 
   vector<vector<Point>> contours;
   vector<Vec4i> hierarchy;
+
   /// Find contours
   findContours(imgThreshold, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
   for (int i = 0; i < contours.size(); i++)
   {
-    drawContours(src, contours, i, Scalar(0, 0, 255), 2, 8, hierarchy, 0, Point());
+    drawContours(bgr_image, contours, i, Scalar(0, 0, 255), 2, 8, hierarchy, 0, Point());
   }
 
   namedWindow("Display Image", WINDOW_AUTOSIZE);
   imshow("Display Image", imgThreshold);
-  imshow("Final result", src);
-
+  imshow("Final result", bgr_image);
   waitKey(0);
 
   return 0;
 }
-
-//https://stackoverflow.com/questions/43981903/using-inrange-in-opencv-to-detect-colors-in-a-range
-
-// Mat redFilter(const Mat &src)
-// {
-//   int H_MIN = 0;
-// 	int H_MAX = 255;
-
-// 	int S_MIN = 0;
-// 	int S_MAX = 0;
-
-// 	int V_MIN = 0;
-// 	int V_MAX = 0;
-
-//   assert(src.type() == CV_8UC3);
-
-//   Mat redOnly;
-//   inRange(src, Scalar (H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), redOnly);
-
-//   return redOnly;
-// }
-
-// int main()
-// {
-
-//   Mat image;
-//   image = imread("/home/jotellybarros/Documents/aaa.png", CV_LOAD_IMAGE_COLOR); // Read the file
-
-//   if (!image.data) // Check for invalid input
-//   {
-//     cout << "Could not open or find the image" << std::endl;
-//     return -1;
-//   }
-
-//   namedWindow("Original Image", CV_WINDOW_AUTOSIZE);
-//   imshow("Original Image", image);
-
-//   Mat redOnly = redFilter(image);
-
-//   imshow("redOnly", redOnly);
-//   waitKey(0); //Wait for a keystroke in the window
-
-//   return 0;
-// }
-// //g++ -std=c++11 main.cpp -lstdc++ $(pkg-config --libs --cflags opencv)
